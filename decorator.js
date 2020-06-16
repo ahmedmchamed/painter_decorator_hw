7d4bdd1eed43acf5704ce95e21ebc9736ae75774
Ahmed Hamed <tunashark@gmail.com>
2020-06-16 18:22:37 +0100

    check if there is enough paint - Decorator

--- decorator.js
+++ decorator.js
@@ -1,5 +1,32 @@
+const Room = require("./room");
+
 const Decorator = function () {
     this.stock = [];
 }
+
+Decorator.prototype.addCanOfPaint = function (paintCan) {
+    this.stock.push(paintCan);
+}
+
+Decorator.prototype.calculateTotalLitres = function () {
+    let totalLitres = 0;
+    for (const paintCan of this.stock) {
+        totalLitres += paintCan.numberOfLitres;
+    }
+    return totalLitres;
+}
+
+Decorator.prototype.checkIfThereIsEnoughPaint = function (room) {
+    const totalLitresOfPaint = this.calculateTotalLitres();
+    return totalLitresOfPaint > room.area;
+}
+
+Decorator.prototype.paintRoomIfEnoughStock = function (room) {
+    if (this.checkIfThereIsEnoughPaint === true) {
+        room.paintRoom(true);
+    }
+}
+
+
 
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -6,11 +6,38 @@ const Room = require('../room.js')
 describe('Decorator', () => {
     let decorator; 
     let paintCan;
-    let room 
+    let room;
 
     beforeEach(() => {
         decorator = new Decorator();
-        paintCan = new PaintCan(5);
-        room = = new Room(5);
+        paintCan1 = new PaintCan(5);
+        paintCan2 = new PaintCan(5);
+        room = new Room(5);
+    })
+
+    it('should check that the stock is empty', () => {
+        const actual = decorator.stock.length;
+        assert.strictEqual(actual, 0);
+    })
+
+    it('should add can of paint to stock', () => {
+        decorator.addCanOfPaint(paintCan1);
+        const actual = decorator.stock.length;
+        assert.strictEqual(actual, 1)
+    })
+
+    it('should check the total number of paint litres in stock', () => {
+        decorator.addCanOfPaint(paintCan1);
+        decorator.addCanOfPaint(paintCan2);
+        const actual = decorator.calculateTotalLitres();
+        assert.strictEqual(actual, 10)
+
+    })
+
+    it('can check if there is enough paint for a room', () => {
+        decorator.addCanOfPaint(paintCan1);
+        decorator.addCanOfPaint(paintCan2);
+        const actual = decorator.checkIfThereIsEnoughPaint(room);
+        assert.strictEqual(actual, true);
     })
 })
