ed043c86a3e294b830de7dd986088d24f9113ffe
Ahmed Hamed <tunashark@gmail.com>
2020-06-16 16:32:33 +0100

    initial paint can spec code

--- specs/paintCan_spec.js
+++ specs/paintCan_spec.js
@@ -1,2 +1,10 @@
 const assert = require('assert')
 const Room = require('../paintCan.js')
+
+describe('Paint Can', () => {
+    let paintCan;
+
+    beforeEach(() => {
+        paintCan = new PaintCan();
+    })
+})
