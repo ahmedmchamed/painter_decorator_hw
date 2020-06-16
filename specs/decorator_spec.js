df9892b894e8c42f8af5303af75b7e3340e960e6
Ahmed Hamed <tunashark@gmail.com>
2020-06-16 16:34:19 +0100

    initial decorator spec code

--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,2 +1,10 @@
 const assert = require('assert')
 const Room = require('../decorator.js')
+
+describe('Decorator', () => {
+    let decorator;
+
+    beforeEach(() => {
+        decorator = new Decorator();
+    })
+})
